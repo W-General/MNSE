cf880d23aca657fbb8e0f6c667900885df94c8a4
formatjam <formatjam@gmail.com>
2013-09-21 18:23:11 -0400

    get communication working

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,9 +4,15 @@ chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
 
-chrome.runtime.onMessage.addListener(
-	function(request){
-		console.log(request);
-	}
-);
 
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	//console.log(request.text);
+  	var words = request.text.split(" ");
+  	for (var i = 0; i < words.length; i++) {
+  		words[i] = "!" + words[i];
+  	}
+  	console.log(words.join(" "));
+    sendResponse({text: words.join(" ")});
+  }
+);
