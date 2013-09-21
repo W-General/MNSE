232a69b8c12b26f360ad16a1a06c7c9e81e8f914
Eric Yu <ekyu@umich.edu>
2013-09-21 16:01:02 -0400

    modded background script

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -3,3 +3,10 @@
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
+
+chrome.runtime.onMessage.addListener(
+	function(request){
+		console.log(request);
+	}
+);
+
