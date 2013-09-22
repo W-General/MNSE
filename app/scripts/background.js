588ca09d7feda947b447b8cd01ba821df114113e
formatjam <formatjam@gmail.com>
2013-09-22 00:47:27 -0400

    loaded word lists and filtered by constructing known_words dict`

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,5 +1,53 @@
 'use strict';
 
+var known_words = {}
+// read in high school word list
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+    if (xhr.readyState != 4)
+        return;
+    var w = JSON.parse(xhr.responseText);
+    for (var i in w.words) {
+      //console.log(w.words[i]);
+      known_words[stemmer(w.words[i])] = 1;
+    }
+    //  console.log(known_words);
+}
+xhr.open("GET", chrome.extension.getURL('/words/highschool.json'), true);
+xhr.send();
+
+
+// read in 4 level word list
+xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+    if (xhr.readyState != 4)
+        return;
+    var w = JSON.parse(xhr.responseText);
+    for (var i in w.words) {
+      //console.log(w.words[i]);
+      known_words[stemmer(w.words[i])] = 1;
+    }
+    //  console.log(known_words);
+}
+xhr.open("GET", chrome.extension.getURL('/words/4level.json'), true);
+xhr.send();
+
+
+
+//see if it's english word
+function is_english(word) {
+  for (var i = 0; i < word.length; i ++) {
+    if ((word[i] >= 'a' && word[i] <= 'z') ||
+      (word[i] >= 'a' && word[i] <= 'z')) {
+    } else {
+      console.log("<3333 : " + word);
+      return false;
+    }
+  }
+  return true;
+}
+
+//communication
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
@@ -10,9 +58,15 @@ chrome.runtime.onMessage.addListener(
   	//console.log(request.text);
   	var words = request.text.split(" ");
   	for (var i = 0; i < words.length; i++) {
-  		//words[i] = "!" + words[i];
+      if (is_english(words[i]) && !known_words[stemmer(words[i])]) {
+        //translate
+        console.log(words[i]);
+        words[i] = words[i] + "!!!!!!!!";
+      }
   	}
-  	console.log(words.join(" "));
+  	//console.log(words.join(" "));
     sendResponse({text: words.join(" ")});
   }
 );
+
+
